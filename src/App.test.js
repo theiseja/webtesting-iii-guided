c5d7bcb6907665ea8713476b74660bb3cc6924d2
Jesse Theisen <theiseja@gmail.com>
2019-04-15 21:14:56 -0500

    MVP: Added snap and mock tests plus some comments

--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,20 @@ import App from './App';
 
 describe('<App />', () => {
   // 2. write this test
-  it('matches snapshot', () => {
+  it('matches snapshot', () => {  // it.skip or it.only also works xit can replace skip
     const tree = renderer.create(<App />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
   });
+
+  // mock test
+
+  it('is mocking me', () => {
+    const mock = jest.fn();
+
+    const result = mock();
+    expect(result).toBeUndefined();
+    expect(mock).toHaveBeenCalled();
+  })
 });
