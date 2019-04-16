4b8107b925d7e6cbaa057a7aa32475202c4a61d5
Jesse Theisen <theiseja@gmail.com>
2019-04-15 21:53:15 -0500

    MVP: Added final tests

--- src/speaker/Speaker.test.js
+++ src/speaker/Speaker.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
 
 import Speaker from './Speaker';
 
 
 describe('<Speaker />', () => {
+    it('should match the snapshot', () => {
+     const tree = renderer.create(<Speaker />); // boilerplate for renderer
+
+     expect(tree.toJSON()).toMatchSnapshot();
+    });
+
+    it('should match the snapshot with a message', () => {
+      const tree = renderer.create(<Speaker message={'test message'} />); // returns a message
+
+      expect(tree.toJSON()).toMatchSnapshot();
+    });
+
   it('should call the speak function on click', () => {  // checks to make sure the speaker function works when you click the button
     const speakMock = jest.fn();
     const { getByText } = render(<Speaker speak={speakMock} />); // renders the Speaker component
