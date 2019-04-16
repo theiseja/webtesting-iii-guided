f61f55736cca9a9ec97c22fcb4a46b85f5edfd5b
Jesse Theisen <theiseja@gmail.com>
2019-04-15 20:57:55 -0500

    MVP: Project setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import banana from 'react-test-renderer'; // 1: install this npm module as a dev dependency
+import renderer from 'react-test-renderer'; // 1: install this npm module as a dev dependency can be named anything
 
 import App from './App';
 
 describe('<App />', () => {
   // 2. write this test
   it('matches snapshot', () => {
-    const tree = banana.create(<App />); // generates a DOM tree
+    const tree = renderer.create(<App />); // generates a DOM tree
 
     // snapshots are a JSON representation of the DOM tree
     expect(tree.toJSON()).toMatchSnapshot();
